92e69283bfc326a603b6732ab45cafbc06f393a7
Elizabeth Karst <lkarst@outlook.com>
2018-08-22 01:16:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+      self.end_with?(".")
   end
 
   def question?
-
+      self.end_with?("?")
   end
 
   def exclamation?
-
+      self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    char_prev = 0
+    self.each_char do |char|
+      if (char == "." || char == "?" || char == "!") && (char_prev != "." && char_prev != "?" && char_prev != "!")
+        count +=1
+      end
+      char_prev = char
+    end
+    count
   end
 end
